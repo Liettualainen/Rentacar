98d78fbe0ccadfa92987c60cab809fefc80c3653
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2023-09-20 21:44:56 +0300

    except favorites

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import { lazy } from "react";
+import { Route, Routes } from 'react-router-dom';
+
+
+
+import { Layout } from "./Layout";
+import { Navigation } from "./Navigation";
+import {
+    AppLayout} from './Styled/AppLayout.styled'
+
+const HomePage = lazy(() => import('./Pages/Home'));
+const Catalogue = lazy(() => import('./Pages/Catalogue'));
+const Favorites = lazy(() => import('./Pages/Favorites'));
+const NotFound = lazy(() => import('./Pages/Notfound'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    <AppLayout>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/catalogue" element={<Catalogue />} />
+        <Route path="/favorites" element={<Favorites />} />          
+        <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </AppLayout>
+  )
 };
